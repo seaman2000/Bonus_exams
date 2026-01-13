c481a7f3c8fed098d877c7a172c67e1e41a4bc6a
Georgi Angelov <gtangelov76@gmail.com>
2026-01-13 03:25:38 +0200

    fixed my_list initialization.

--- wolf_in_sheeps_clothing.py
+++ wolf_in_sheeps_clothing.py
@@ -1,6 +1,6 @@
 sheep_or_wolf = input().split(", ")
-mylist = sheep_or_wolf[::-1]
-for idx, word in enumerate(mylist):
+my_list = sheep_or_wolf[::-1]
+for idx, word in enumerate(my_list):
     if word == "wolf" and idx == 0:
         print("Please go away and stop eating my sheep")
         break
